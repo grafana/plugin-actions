d3d51532aa535daf35be83ed89f64e4131678b79
Jack Westbrook <jack.westbrook@gmail.com>
2024-11-25 15:03:54 +0100

    Feat: Bundle size action (#41)
    * docs(bundle-size): add license and readme for action
    
    * feat(bundle-size): add script that diffs  webpack stats and posts PR comments
    
    * feat(bundle-size): add composite workflow
    
    * feat(bundle-size): check for pull_request, update readme
    
    * fix(bundle-size): prevent errors by selecting the latest artifact if multiple are returned by api
    
    * feat(bundle-size): bail early if action event is not pull_request

---
+++ bundle-size/pm.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Check if command argument is provided
+if [ "$1" = "" ]; then
+	echo "Please provide a command to run."
+	exit 1
+fi
+
+install_pnpm_if_not_present() {
+    if ! command -v pnpm &> /dev/null
+    then
+        echo "pnpm could not be found, installing..."
+        npm install -g pnpm
+    fi
+}
+
+# Detect the package manager
+if [ -f yarn.lock ]; then
+	pm="yarn"
+elif [ -f pnpm-lock.yaml ]; then
+	install_pnpm_if_not_present
+	pm="pnpm"
+elif [ -f package-lock.json ]; then
+	pm="npm"
+else
+	echo "No recognized package manager found in this project."
+	exit 1
+fi
+
+# Detect the command to run build
+if [ -f yarn.lock ]; then
+  pmb=("yarn" "build" "--profile" "--json" "pr-stats.json")
+elif [ -f pnpm-lock.yaml ]; then
+  install_pnpm_if_not_present
+  pmb=("pnpm" "build" "--profile" "--json" "pr-stats.json")
+elif [ -f package-lock.json ]; then
+  pmb=("npm" "run" "build" "--" "--profile" "--json" "pr-stats.json")
+else
+  echo "No recognized package manager found in this project."
+  exit 1
+fi
+
+# Run the provided command with the detected package manager
+echo "Running '$1' with $pm..."
+if [ "$1" = "install" ]; then
+	"$pm" install
+elif [ "$1" = "build" ]; then
+	"${pmb[@]}"
+fi

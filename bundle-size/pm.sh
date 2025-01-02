c6c62b647df63a1e2952751ace71aa08590d0fd1
Dominik Broj <dominik.broj@grafana.com>
2025-01-02 11:15:35 +0100

    Feat: Support monorepos (#45)
    * chore: support working-directory input
    
    * default to pnpm if lock file not found
    
    * remove exit
    
    * update
    
    * support main-branch-stats-artifact input
    
    * update pr-stats.json path to support working-directory input
    
    * print subdirectory in comment title if exists
    
    * support comment per working directory
    
    * add title to below threshold comment
    
    * remove duplicated dash

--- bundle-size/pm.sh
+++ bundle-size/pm.sh
@@ -17,27 +17,23 @@ install_pnpm_if_not_present() {
 # Detect the package manager
 if [ -f yarn.lock ]; then
 	pm="yarn"
-elif [ -f pnpm-lock.yaml ]; then
-	install_pnpm_if_not_present
-	pm="pnpm"
 elif [ -f package-lock.json ]; then
 	pm="npm"
 else
-	echo "No recognized package manager found in this project."
-	exit 1
+	echo "Defaulting to pnpm for install command."
+	install_pnpm_if_not_present
+	pm="pnpm"
 fi
 
 # Detect the command to run build
 if [ -f yarn.lock ]; then
   pmb=("yarn" "build" "--profile" "--json" "pr-stats.json")
-elif [ -f pnpm-lock.yaml ]; then
-  install_pnpm_if_not_present
-  pmb=("pnpm" "build" "--profile" "--json" "pr-stats.json")
 elif [ -f package-lock.json ]; then
   pmb=("npm" "run" "build" "--" "--profile" "--json" "pr-stats.json")
 else
-  echo "No recognized package manager found in this project."
-  exit 1
+  echo "Defaulting to pnpm for build command."
+  install_pnpm_if_not_present
+  pmb=("pnpm" "build" "--profile" "--json" "pr-stats.json")
 fi
 
 # Run the provided command with the detected package manager

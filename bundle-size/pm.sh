85936c93cd2d874f71320585e43cb86a993b1ea5
Dominik <dominik.broj@grafana.com>
2025-01-02 15:05:49 +0100

    chore: support nonpnpm monorepos

--- bundle-size/pm.sh
+++ bundle-size/pm.sh
@@ -15,25 +15,32 @@ install_pnpm_if_not_present() {
 }
 
 # Detect the package manager
+current_dir=$(pwd)
+cd $(git rev-parse --show-toplevel) # Navigate to the root of the git repository
 if [ -f yarn.lock ]; then
 	pm="yarn"
 elif [ -f package-lock.json ]; then
 	pm="npm"
+elif [ -f pnpm-lock.yaml ]; then
+  install_pnpm_if_not_present
+  pm="pnpm"
 else
-	echo "Defaulting to pnpm for install command."
-	install_pnpm_if_not_present
-	pm="pnpm"
+  echo "No recognized package manager found in this project."
+	exit 1
 fi
+cd $current_dir # Navigate back to the original directory
 
 # Detect the command to run build
-if [ -f yarn.lock ]; then
+if [ "$pm" = "yarn" ]; then
   pmb=("yarn" "build" "--profile" "--json" "pr-stats.json")
-elif [ -f package-lock.json ]; then
+elif [ "$pm" = "npm" ]; then
   pmb=("npm" "run" "build" "--" "--profile" "--json" "pr-stats.json")
-else
-  echo "Defaulting to pnpm for build command."
+elif [ "$pm" = "pnpm" ]; then
   install_pnpm_if_not_present
   pmb=("pnpm" "build" "--profile" "--json" "pr-stats.json")
+else
+  echo "No recognized package manager found in this project."
+	exit 1
 fi
 
 # Run the provided command with the detected package manager

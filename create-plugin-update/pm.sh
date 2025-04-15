d355118d0cbcff99640815c148dd42a161cb0b9c
Matt Cowley <me@mattcowley.co.uk>
2025-04-15 09:35:11 +0100

    Create Plugin Update: Fix pnpm install + update_needed check (#56)
    * Combine pm + pmx detection into single block
    
    * Add --no-frozen-lockfile arg for pnpm install
    
    * Log GitHub output writes for easier debugging
    
    * Trim trailing whitespace from LATEST_VERSION
    
    * Reduce interpolation used for commands
    
    * Less cryptic names for script arrays
    
    * Revert tee debugging changes

--- create-plugin-update/pm.sh
+++ create-plugin-update/pm.sh
@@ -15,36 +15,28 @@ install_pnpm_if_not_present() {
 }
 
 # Detect the package manager
+# to determine the correct installation command
+# and the exec command to run create-plugin update
 if [ -f yarn.lock ]; then
-	pm="yarn"
+	install_deps=("yarn" "install")
+	create_plugin_update=("yarn" "create" "@grafana/plugin" "update")
 elif [ -f pnpm-lock.yaml ]; then
 	install_pnpm_if_not_present
-	pm="pnpm"
+	install_deps=("pnpm" "install" "--no-frozen-lockfile")
+	create_plugin_update=("pnpm" "dlx" "@grafana/create-plugin@latest" "update")
 elif [ -f package-lock.json ]; then
-	pm="npm"
+	install_deps=("npm" "install")
+	create_plugin_update=("npx" "-y" "@grafana/create-plugin@latest" "update")
 else
 	echo "No recognized package manager found in this project."
 	exit 1
 fi
 
-# Detect the exec command to run create-plugin update
-if [ -f yarn.lock ]; then
-  pmx=("yarn" "create" "@grafana/plugin")
-elif [ -f pnpm-lock.yaml ]; then
-  install_pnpm_if_not_present
-  pmx=("pnpm" "dlx" "@grafana/create-plugin@latest")
-elif [ -f package-lock.json ]; then
-  pmx=("npx" "-y" "@grafana/create-plugin@latest")
-else
-  echo "No recognized package manager found in this project."
-  exit 1
-fi
-
 # Run the provided command with the detected package manager
 if [ "$1" = "install" ]; then
-  echo "Running '$1' with $pm..."
-	"$pm" install
+	echo "Running '$1' with ${install_deps[0]}..."
+	"${install_deps[@]}"
 elif [ "$1" = "update" ]; then
-  echo "Running '$1' with ${pmx[0]}..."
-	"${pmx[@]}" update
+	echo "Running '$1' with ${create_plugin_update[0]}..."
+	"${create_plugin_update[@]}"
 fi

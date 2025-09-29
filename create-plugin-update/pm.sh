677c147c0df4113fc6f6e3dd5747659098f0f3a2
Jack Westbrook <jack.westbrook@gmail.com>
2025-09-29 12:17:06 +0200

    feat(create-plugin-update): support create-plugin v6 commit flag

--- create-plugin-update/pm.sh
+++ create-plugin-update/pm.sh
@@ -19,14 +19,14 @@ install_pnpm_if_not_present() {
 # and the exec command to run create-plugin update
 if [ -f yarn.lock ]; then
 	install_deps=("yarn" "install" "--no-immutable")
-	create_plugin_update=("yarn" "create" "@grafana/plugin" "update")
+	create_plugin_update=("yarn" "create" "@grafana/plugin" "update" "--commit")
 elif [ -f pnpm-lock.yaml ]; then
 	install_pnpm_if_not_present
 	install_deps=("pnpm" "install" "--no-frozen-lockfile")
-	create_plugin_update=("pnpm" "dlx" "@grafana/create-plugin@latest" "update")
+	create_plugin_update=("pnpm" "dlx" "@grafana/create-plugin@latest" "update" "--commit")
 elif [ -f package-lock.json ]; then
 	install_deps=("npm" "install")
-	create_plugin_update=("npx" "-y" "@grafana/create-plugin@latest" "update")
+	create_plugin_update=("npx" "-y" "@grafana/create-plugin@latest" "update" "--commit")
 else
 	echo "No recognized package manager found in this project."
 	exit 1

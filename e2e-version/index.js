9c118af3543847a7da2fe1b881a92519d421fdca
Giuseppe Guerra <giuseppe@guerra.in>
2026-06-18 17:45:39 +0200

    Revert "simulate change"
    This reverts commit f12ea6c785eaab41b8f77eb305bd8d69e074e2df.

--- e2e-version/index.js
+++ e2e-version/index.js
@@ -110,7 +110,7 @@ function evenlyPickVersions(allItems, limit) {
 async function getGrafanaStableMinorVersions() {
   const latestMinorVersions = new Map();
 
-  const response = await fetch('https://grafana.com/api/grafana-enterprise/versions/TESTTEST');
+  const response = await fetch('https://grafana.com/api/grafana-enterprise/versions');
   const json = await response.json();
   const grafanaVersions = json.items;
 
